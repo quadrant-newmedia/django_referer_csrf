ebbeb83afa4f2e2e70766ff110c586d1e03689c0
Alex Fischer <alex@quadrant.net>
2024-11-20 10:27:27 -0600

    Explicit Django 5 support

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,6 @@
+## 0.2.0
+Explicit Django 5 support
+
 # 0.0.1
 Explicitly support django 3.1, 3.2
 
--- setup.py
+++ setup.py
@@ -5,19 +5,19 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django_referer_csrf",
-    version="0.1.0",
+    version="0.2.0",
     author="Alex Fischer",
     author_email="alex@quadrant.net",
     description="TODO",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/quadrant-newmedia/django_referer_csrf",
-    packages=['django_referer_csrf', 'django_referer_csrf.tests'],
+    packages=["django_referer_csrf", "django_referer_csrf.tests"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
-    install_requires=["Django>=2.2,<4.3"],
+    python_requires=">=3.7",
+    install_requires=["Django>=3,<6"],
 )
